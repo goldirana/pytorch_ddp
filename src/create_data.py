2b39136dd41649e96146bd1be32713c08664f7c3
goldirana <goldirana3210@gmail.com>
2025-01-10 20:09:09 +0530

    added data

---
+++ src/create_data.py
@@ -0,0 +1,17 @@
+import torch 
+from torch.utils.data import Dataset
+
+class my_data(Dataset):
+    def __init__(self, size):
+        self.size=size
+        self.data = [(torch.rand(20), torch.rand(1)) for _ in range(size)]
+    
+    def __len__(self):
+        return self.size
+    
+    def __getitem__(self, index):  
+        return self.data[index]
+    
+if __name__ == "__main__":
+    data = my_data(1000)
+    
