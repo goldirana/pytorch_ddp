0c467c9167d9dd3a9c6ce54a75b0fc6af58a2b7c
goldyrana <goldirana3210@gmail.com>
2025-01-12 21:39:45 +0530

    updated ddp.py

--- src/create_data.py
+++ src/create_data.py
@@ -3,9 +3,10 @@ from torch.utils.data import Dataset
 
 class my_data(Dataset):
     def __init__(self, size):
-        self.size=size
+        self.size = size
         self.data = [(torch.rand(20), torch.rand(1)) for _ in range(size)]
-    
+        self.label = torch.rand(size)
+        
     def __len__(self):
         return self.size
     
